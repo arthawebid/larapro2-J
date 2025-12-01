8d8f4c2dc40f6ff0b4159a3dcd52adad2ca7ed62
MhSTIKI <arthawebid@gmail.com>
2025-12-01 14:08:55 +0800

    update

---
+++ app/Http/Controllers/temansController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\facades\DB;
+
+class temansController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $dta = DB::table('temans')->get();
+        return view('temans.index',compact('dta')); 
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('temans.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // validasi
+        $request->validate([
+            'nama' => 'required',
+            'alamat' => 'required',
+            'kota' => 'required',
+            'noTelp' => 'required'
+        ]);
+
+        // insert data
+        DB::table('temans')->insert([
+            'nama'   => $request->nama,
+            'alamat' => $request->alamat,
+            'kota'   => $request->kota,
+            'noTelp' => $request->noTelp,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        return redirect('/temans')->with('success', 'Data berhasil ditambahkan!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/temans/index.blade.php
@@ -0,0 +1,25 @@
+<div class="container">
+    <h3>Data Teman</h3>
+
+    <table>      
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($dta as $dt)
+            <tr>
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $dt['nama'] }}</td>
+                <td><a href="{{ route('teman.edit', $dt['id'])  }}"> Ubah </a> | Hapus</td>
+            </tr>
+            @endforeach
+
+        </tbody>
+
+    </table>
+
+</div>
